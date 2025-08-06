a942f79bdbc6217e436cd6dcce0befaf81800559
f3rn4nd0l0 <l.loaa050209@itses.edu.mx>
2025-08-05 21:42:59 -0600

    Seccion.java

--- src/main/java/mx/edu/itses/electricidad/aflda/seccion/Seccion.java
+++ src/main/java/mx/edu/itses/electricidad/aflda/seccion/Seccion.java
@@ -38,23 +38,19 @@ public class Seccion {
 
     @Override
     public void start(Stage primaryStage) {
-        // Inicializar residencias
-        // Nota: Las residencias se crean con un radio de 10. Los clics se gestionan en base a estos círculos.
+
         Residencia res1 = new Residencia("Residencia A", 300, 150, 150);
         Residencia res2 = new Residencia("Residencia B", 500, 500, 250);
         Residencia res3 = new Residencia("Residencia C", 200, 300, 450);
 
         allResidences.addAll(Arrays.asList(res1, res2, res3));
 
-        // Configurar el grupo de estudio (mínimo 3 personas, 1 de otra residencia)
-        studyGroup.addAll(Arrays.asList(res1, res2, res3)); // Ejemplo: una persona de cada residencia
+        studyGroup.addAll(Arrays.asList(res1, res2, res3)); 
 
-        // Configurar el panel principal
         Pane mapPane = new Pane();
         mapPane.setPrefSize(800, 600);
         mapPane.setStyle("-fx-background-color: lightgrey;");
 
-        // Agregar residencias y sus etiquetas al panel
         for (Residencia res : allResidences) {
             Circle resCircle = res.getCircle();
             resCircle.setFill(Color.RED);
@@ -68,7 +64,6 @@ public class Seccion {
                 updateCentralPoints();
             });
 
-            // Hacer que las residencias sean arrastrables
             resCircle.setOnMouseDragged(e -> {
                 resCircle.setCenterX(e.getX());
                 resCircle.setCenterY(e.getY());
@@ -80,16 +75,13 @@ public class Seccion {
             mapPane.getChildren().addAll(resCircle, label);
         }
 
-        // Agregar puntos centrales y sus etiquetas
         allResidencesCenter.setFill(Color.BLUE);
         studyGroupCenter.setFill(Color.GREEN);
 
         mapPane.getChildren().addAll(allResidencesCenter, studyGroupCenter, allResidencesCenterLabel, studyGroupCenterLabel);
 
-        // Actualizar puntos iniciales
         updateCentralPoints();
 
-        // Configurar UI para ajustar la población
         VBox controls = new VBox(10);
         controls.setPadding(new Insets(10));
         
@@ -113,7 +105,6 @@ public class Seccion {
     }
 
     private void updateCentralPoints() {
-        // Calcular y actualizar el centro de todas las residencias
         double allResX = CentralPointCalculator.calculateCenterX(allResidences);
         double allResY = CentralPointCalculator.calculateCenterY(allResidences);
         allResidencesCenter.setCenterX(allResX);
@@ -122,7 +113,6 @@ public class Seccion {
         allResidencesCenterLabel.setY(allResY - 10);
         allResidencesCenterLabel.setText(String.format("Centro de todas las residencias: (%.2f, %.2f)", allResX, allResY));
 
-        // Calcular y actualizar el centro del grupo de estudio
         double studyGroupX = CentralPointCalculator.calculateCenterX(studyGroup);
         double studyGroupY = CentralPointCalculator.calculateCenterY(studyGroup);
         studyGroupCenter.setCenterX(studyGroupX);
